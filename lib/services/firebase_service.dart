7dfb28f26716a16a6c9c2035aa6726b61ec35d26
YusufEbinc <86497681+YusufEbinc@users.noreply.github.com>
2022-03-26 20:23:47 +0300

    veri çekme işlemi gerçekleştirildi

--- lib/services/firebase_service.dart
+++ lib/services/firebase_service.dart
@@ -1,25 +1,57 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/widgets.dart';
+import 'package:intl/intl.dart';
 import 'package:personel_app/model/users_data_model.dart';
 
-class FirebaseService extends ChangeNotifier {
+class FirebaseService {
   final FirebaseFirestore _firebaseFirestore = FirebaseFirestore.instance;
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  Future<dynamic> addData(UsersData usersData) async {
+  Stream<QuerySnapshot> getIndex(int index) {
+    switch (index) {
+      case 0:
+        return FirebaseFirestore.instance
+            .collection('yusuf@gmail.com')
+            .snapshots();
+      case 1:
+        return FirebaseFirestore.instance
+            .collection('yusuf@gmail.com')
+            .orderBy('price', descending: false)
+            .snapshots();
+      case 2:
+        return FirebaseFirestore.instance
+            .collection('yusuf@gmail.com')
+            .orderBy('price', descending: true)
+            .snapshots();
+      case 3:
+        return FirebaseFirestore.instance
+            .collection('yusuf@gmail.com')
+            .where(
+              'paymnet',
+            )
+            .snapshots();
+
+      default:
+        return FirebaseFirestore.instance
+            .collection('yusuf@gmail.com')
+            .snapshots();
+    }
+  }
+
+  Future<void> addData(UsersData usersData) async {
     try {
       await _firebaseFirestore
-          .collection('Usersdd')
-          .doc(_auth.currentUser!.email)
-          .set({
-        'expenses': usersData.expenses,
-        'payment': usersData.paymentType,
-        'price': usersData.price,
-        'date': usersData.date
-      });
+          .collection(_auth.currentUser!.email!)
+          .add(usersData.toMap());
     } on FirebaseException catch (error) {
-      return print('error: ${error.toString()}');
+      return debugPrint('error: ${error.toString()}');
     }
+  }
+
+  String convertTimeStamp(Timestamp timestamp) {
+    String convertedDate;
+    convertedDate = DateFormat.yMMMd().format(timestamp.toDate());
+    return convertedDate;
   }
 }
